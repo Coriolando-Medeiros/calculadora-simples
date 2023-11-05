30ba7086b63b627f6986faea89feb0b0828bafc5
Coriolando Medeiros <coriolando.medeiros@gmail.com>
2023-11-05 08:22:50 -0300

    Adicionado as funções Potência e raiz n-ésima e criado a função entradas

--- calculadora.py
+++ calculadora.py
@@ -21,47 +21,61 @@ def multiplicacao(num1, num2):
 def divisao(num1, num2):
     total = num1/num2
     return total
+
+def entradas():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    return num1, num2
     
+while True:
+    print('                           Calculadora')
+    print('')
+    print('                                 Menu')
+    print('------------------------------------------------------------------')
+    print('|                                                                |')
+    print('|                            1 -> Soma                           |')
+    print('|                            2 -> Subtração                      |')
+    print('|                            3 -> Multiplicação                  |')
+    print('|                            4 -> Divisão                        |')
+    print('|                            5 -> Raiz quad.                     |')
+    print('|                            6 -> Potência                       |')
+    print('|                                                                |')
+    print('|                            0 -> Sair                           |')
+    print('------------------------------------------------------------------')
+    print('')
+    opcao = int(input('Operação: '))
+    if opcao == 1:
+        num1, num2 = entradas()
+        resultado = soma(num1, num2)
+        print(f'{num1} + {num2} = {resultado:.2f}')
 
-print('                           Calculadora')
-print('')
-print('                                 Menu')
-print('------------------------------------------------------------------')
-print('|                            1 -> Soma                           |')
-print('|                            2 -> Subtração                      |')
-print('|                            3 -> Multiplicação                  |')
-print('|                            4 -> Divisão                        |')
-print('|                                                                |')
-print('|                            0 -> Sair                           |')
-print('------------------------------------------------------------------')
-opcao = int(input('Operação: '))
-num1 = float(input('Digite o primeiro número: '))
-num2 = float(input('Digite o segundo número: '))
+    elif opcao == 2:
+        num1, num2 = entradas()
+        resultado = subtracao(num1, num2)
+        print(f'{num1} - {num2} = {resultado}')
 
-if opcao == 1:
-    resultado = soma(num1, num2)
-    print(f'{num1} + {num2} = {resultado:.2f}')
+    elif opcao == 3:
+        num1, num2 = entradas()
+        resultado = multiplicacao(num1, num2)
+        print(f'{num1} * {num2} = {resultado}')
 
-elif opcao == 2:
-    resultado = subtracao(num1, num2)
-    print(f'{num1} - {num2} = {resultado}')
-
-elif opcao == 3:
-    resultado = multiplocacao(num1, num2)
-    print(f'{num1} * {num2} = {resultado}')
-
-elif opcao == 4:
-    resultado = multiplicacao(num1, num2)
-    print(f'{num1} + {num2} = {resultado}')
-elif opcao == 0:
-    resultado = raiz(num1, num2)
-    print(f'{num2}  ✓  {num1} = {resultado}')
-#    print('Obrigado por usar nossos sistemas!')
-#    print('Volte sempre!')
- #   break
-
-#if opcao == 0:
- #   print(f'Obrigado por usar nossa aplicação!')
-#break
-#resultado = potencia(num1, num2)
-#print(resultado)
+    elif opcao == 4:
+        num1, num2 = entradas()
+        resultado = divisao(num1, num2)
+        print(f'{num1} / {num2} = {resultado}')
+    elif opcao == 5:
+        num1 = float(input('Digite o número: '))
+        num2 = float(input('Digite o expoente da raiz: '))
+        resultado = raiz(num1, num2)
+        print(f'{num1} √ {num2} = {resultado}')
+    elif opcao == 6:
+        num1 = float(input('Digite a base: '))
+        num2 = float(input('Digite o expoente da potência: '))
+        resultado = potencia(num1, num2)
+        print(f'{num1} ^ {num2} = {resultado}')
+    elif opcao == 0:
+        print('Obrigado por usar nossos sistemas!')
+        print('Volte sempre!')
+        break
+    else:
+        print('Opção inválida! Tente novamente.')
