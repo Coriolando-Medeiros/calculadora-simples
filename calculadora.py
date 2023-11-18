a29a5d80f77ba4fe8bdc4e71c4af5b9d0920146c
GleisonAmorim <54336609+GleisonAmorim@users.noreply.github.com>
2023-11-17 22:58:38 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,177 +1,65 @@
-def potencia(num1, num2):
-    total = num1 ** num2
-    return total
-    
-def raiz(num1, num2):
-    total = num1 ** (1/num2)
-    return total
-    
-def soma(num1, num2):
-    total = num1 + num2
-    return total
-    
-def subtracao(num1, num2):
-    total = num1 - num2
-    return total
-    
-def multiplicacao(num1, num2):
-    total = num1 * num2
-    return total
-    
-def divisao(num1, num2):
-        try:
-            total = num1/num2
-        except ZeroDivisionError:
-            print("Erro! Não é possível dividir por zero.")
-        total = 0
-        return total
+import tkinter as tk
+from tkinter import ttk, messagebox
 
-def entradas():
-    num1 = float(input('Digite o primeiro número: '))
-    num2 = float(input('Digite o segundo número: '))
-    return num1, num2
+class Calculadora:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Calculadora")
+        self.root.geometry("400x500")
+        self.root.resizable(False, False)
+        
+        self.style = ttk.Style()
+        self.style.configure("TButton", font=('Arial', 14), padding=10)
 
-def menu_principal():
-    print('')
-    print('                           Calculadora')
-    print('')
-    print('                                Menu')
-    print('------------------------------------------------------------------')
-    print('|                                                                |')
-    print('|                            1 -> Calc. Científica               |')
-    print('|                            2 -> Calc. Padrão                   |')
-    print('|                                                                |')
-    print('|                            0 -> Sair                           |')
-    print('------------------------------------------------------------------')
-    print('')
+        self.frame = ttk.Frame(self.root)
+        self.frame.pack(padx=10, pady=10)
 
-def menu_cient():
-    print('')
-    print('                           Calculadora Científica')
-    print('')
-    print('                                Menu')
-    print('------------------------------------------------------------------')
-    print('|                                                                |')
-    print('|                            1 -> Soma                           |')
-    print('|                            2 -> Subtração                      |')
-    print('|                            3 -> Multiplicação                  |')
-    print('|                            4 -> Divisão                        |')
-    print('|                            5 -> Raiz quad.                     |')
-    print('|                            6 -> Potência                       |')
-    print('|                                                                |')
-    print('|                            0 -> Sair                           |')
-    print('------------------------------------------------------------------')
-    print('')
+        self.resultado_var = tk.StringVar()
+        self.resultado_var.set('')
+        
+        self.entry_resultado = ttk.Entry(self.frame, textvariable=self.resultado_var, state='disabled', justify='right', font=('Arial', 20))
+        self.entry_resultado.grid(row=0, column=0, columnspan=4, pady=5, sticky='nsew')
 
-def menu_padrao():
-    print('')
-    print('                           Calculadora Padrão')
-    print('')
-    print('                                Menu')
-    print('------------------------------------------------------------------')
-    print('|                                                                |')
-    print('|                            1 -> Soma                           |')
-    print('|                            2 -> Subtração                      |')
-    print('|                            3 -> Multiplicação                  |')
-    print('|                            4 -> Divisão                        |')
-    print('|                                                                |')
-    print('|                            0 -> Sair                           |')
-    print('------------------------------------------------------------------')
-    print('')
-    
-while True:
-    menu_principal()
-    opcao = int(input('Operação: '))
-    if opcao == 1:
-        while True:
-            menu_cient()
-            opcao = int(input('Operação: '))
-            if opcao == 1:
-                num1, num2 = entradas()
-                resultado = soma(num1, num2)
-                print(f'{num1} + {num2} = {resultado:.2f}')
-                input('Pressione ENTER para continuar...')
+        self.criar_botoes()
 
-            elif opcao == 2:
-                num1, num2 = entradas()
-                resultado = subtracao(num1, num2)
-                print(f'{num1} - {num2} = {resultado}')
-                input('Pressione ENTER para continuar...')
+        self.root.bind('<Key>', self.tecla_pressionada)
 
-            elif opcao == 3:
-                num1, num2 = entradas()
-                resultado = multiplicacao(num1, num2)
-                print(f'{num1} * {num2} = {resultado}')
-                input('Pressione ENTER para continuar...')
+    def criar_botoes(self):
+        botoes = [
+            ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
+            ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
+            ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+            ('0', 4, 0), ('C', 4, 1), ('=', 4, 2), ('+', 4, 3),
+        ]
 
-            elif opcao == 4:
-                num1, num2 = entradas()
-                resultado = divisao(num1, num2)
-                print(f'{num1} / {num2} = {resultado}')
-                input('Pressione ENTER para continuar...')
-                
-            elif opcao == 5:
-                num1 = float(input('Digite o número: '))
-                num2 = float(input('Digite o expoente da raiz: '))
-                resultado = raiz(num1, num2)
-                print(f'{num1} √ {num2} = {resultado}')
-                input('Pressione ENTER para continuar...')
-                
-            elif opcao == 6:
-                num1 = float(input('Digite a base: '))
-                num2 = float(input('Digite o expoente da potência: '))
-                resultado = potencia(num1, num2)
-                print(f'{num1} ^ {num2} = {resultado}')
-                input('Pressione ENTER para continuar...')
-                
-            elif opcao == 0:
-                print('Voltando para menu principal')
-                break
-            else:
-                print('Opção inválida! Tente novamente.')
-    elif opcao ==2:
-            while True:
-                menu_padrao()
-                opcao = int(input('Operação: '))
-                if opcao == 1:
-                    num1, num2 = entradas()
-                    resultado = soma(num1, num2)
-                    print(f'{num1} + {num2} = {resultado:.2f}')
-                    input('Pressione ENTER para continuar...')
+        for (text, row, column) in botoes:
+            btn = ttk.Button(self.frame, text=text, command=lambda t=text: self.clique_botao(t))
+            btn.grid(row=row, column=column, sticky='nsew')
 
-                elif opcao == 2:
-                    num1, num2 = entradas()
-                    resultado = subtracao(num1, num2)
-                    print(f'{num1} - {num2} = {resultado}')
-                    input('Pressione ENTER para continuar...')
+        for i in range(5):
+            self.frame.grid_rowconfigure(i, weight=1)
+            self.frame.grid_columnconfigure(i, weight=1)
 
-                elif opcao == 3:
-                    num1, num2 = entradas()
-                    resultado = multiplicacao(num1, num2)
-                    print(f'{num1} * {num2} = {resultado}')
-                    input('Pressione ENTER para continuar...')
+    def clique_botao(self, valor):
+        if valor == 'C':
+            self.resultado_var.set('')
+        elif valor == '=':
+            try:
+                expressao = self.resultado_var.get()
+                resultado = eval(expressao)
+                self.resultado_var.set(str(resultado))
+            except Exception as e:
+                messagebox.showerror("Erro", f"Erro na expressão: {e}")
+                self.resultado_var.set('')
+        else:
+            expressao_atual = self.resultado_var.get()
+            self.resultado_var.set(expressao_atual + valor)
 
-                elif opcao == 4:
-                    num1, num2 = entradas()
-                    resultado = divisao(num1, num2)
-                    print(f'{num1} / {num2} = {resultado}')
-                    input('Pressione ENTER para continuar...')
-                    
-                elif opcao == 5:
-                    num1 = float(input('Digite o número: '))
-                    num2 = float(input('Digite o expoente da raiz: '))
-                    resultado = raiz(num1, num2)
-                    print(f'{num1} √ {num2} = {resultado}')
-                    input('Pressione ENTER para continuar...')
-                    
-                elif opcao == 0:
-                    print('Voltando para menu principal')
-                    break
-                else:
-                    print('Opção inválida! Tente novamente.')
-    elif opcao == 0:
-        print('Obrigado por usar nossos sistemas!')
-        print('Volte sempre!')
-        break
-    else:
-        print('Opção inválida! Tente novamente.')
+    def tecla_pressionada(self, event):
+        if event.char.isdigit() or event.char in ['+', '-', '*', '/', '=', 'C']:
+            self.clique_botao(event.char)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Calculadora(root)
+    root.mainloop()
